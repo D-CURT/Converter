0ac871880b5c09dd99723d267d4e2e7807b2d79c
D-CURT <36221315+D-CURT@users.noreply.github.com>
2018-09-13 07:57:57 +0300

    Converter class added;

--- src/DataReader.java
+++ src/DataReader.java
@@ -1,18 +1,28 @@
 import beans.Essence;
 import beans.Speed;
+import beans.Time;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+
+import static beans.enums.TimeUnits.isTimeUnit;
 
 public class DataReader {
     private String path;
 
     public DataReader(String path) {
         this.path = path;
+    }
+
+    public Essence readLine(String s) {
+        String[] separated = s.split(" ");
+        if (isTimeUnit(separated[1])) {
+            return new Time(separated[0], separated[1]);
+        }
+        return new Speed(separated[0], separated[1]);
     }
 
     public List<Speed> getEssenceList() {
---
+++ src/services/Converter.java
@@ -0,0 +1,16 @@
+package services;
+
+import beans.Speed;
+import beans.Time;
+
+import java.util.List;
+
+public class Converter {
+    private List<Speed> list;
+    private Time time;
+
+    public Converter(List<Speed> list, Time time) {
+        this.list = list;
+        this.time = time;
+    }
+}
