b0d3b01f431137acbb58991ffe20133f7a100ee2
D-CURT <36221315+D-CURT@users.noreply.github.com>
2018-09-13 08:23:49 +0300

    DataReader updated;

--- src/beans/Essence.java
+++ src/beans/Essence.java
@@ -4,14 +4,6 @@ public abstract class Essence {
     private String value;
     private String unit;
 
-    public String getValue() {
-        return value;
-    }
-
-    public String getUnit() {
-        return unit;
-    }
-
     @Override
     public String toString() {
         return value + "_in_" + unit;
