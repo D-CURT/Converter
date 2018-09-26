cba037511c30c343b5afc26357d472e3b1a6697e
D-CURT <36221315+D-CURT@users.noreply.github.com>
2018-09-26 20:31:48 +0300

    In progress;

--- src/support/sections/ConverterServices.java
+++ src/support/sections/ConverterServices.java
@@ -1,5 +1,18 @@
 package support.sections;
 
+import beans.Speed;
+import beans.enums.SpeedUnits;
+
+import java.util.function.Function;
+
 public enum ConverterServices {
-    TO_MS
+    TO_MS(SpeedUnits::unitIn_ms);
+
+    private Function<Speed, ?> function;
+
+    ConverterServices(Function<Speed, ?> function) {
+        this.function = function;
+    }
+
+
 }
