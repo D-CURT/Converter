71f239bb572f34375fc0dc2f6eed15aa39b6ab2f
D-CURT <36221315+D-CURT@users.noreply.github.com>
2018-10-23 12:44:48 +0300

    AbstractController servlet methods modified to final;

--- src/controllers/AbstractController.java
+++ src/controllers/AbstractController.java
@@ -9,12 +9,12 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class AbstractController extends HttpServlet {
-    void forward(String url, HttpServletRequest request, HttpServletResponse response)
+    final void forward(String url, HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         getServletContext().getRequestDispatcher(url).forward(request, response);
     }
 
-    void forwardError(String url, String message, HttpServletRequest request, HttpServletResponse response)
+    final void forwardError(String url, String message, HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         request.setAttribute(Constants.ERROR, message);
         getServletContext().getRequestDispatcher(url).forward(request, response);
